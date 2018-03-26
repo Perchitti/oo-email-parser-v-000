09fca58c289f9c44373241d49497c59bc8659cab
Lauren Perchitti <laurenperchitti@gmail.com>
2018-03-26 23:12:46 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -6,14 +6,13 @@
 class EmailParser
   attr_reader :emails
 
-  def initalize(emails)
+  def initialize(emails)
     @emails = emails
   end
 
   def parse
     emails.split.map do |email|
-      email.split (',')
-  end.flatten.uniq
-end
-
+      email.split(',')
+    end.flatten.uniq
+  end
 end
